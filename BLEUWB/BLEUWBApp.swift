658c7bf98b0e1c778d6071f196d40df00753cf4d
kei224022 <ishikawa224022@protonmail.com>
2023-10-24 15:00:35 +0900

    Initial Commit

---
+++ BLEUWB/BLEUWBApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEUWBApp.swift
+//  BLEUWB
+//
+//  Created by member on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BLEUWBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
